b323da3fa6ea9926e47f3937236af81d355abfe5
rakesh <rakeshinsg.gmail.com>
2020-06-22 06:22:57 +0530

    resolved conflict

--- src/main/java/com/rak/assignment/domain/User.java
+++ src/main/java/com/rak/assignment/domain/User.java
@@ -3,15 +3,8 @@ package com.rak.assignment.domain;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@Setter
-@Getter
-@NoArgsConstructor
-@AllArgsConstructor
+
 @Entity(name="USERS")
 public class User {
 	@Id
@@ -20,4 +13,38 @@ public class User {
 	private String userName;
 	private String password;
 	private String role;
+	
+	public User(){}
+	
+	public User(Long uid, String userName, String password, String role) {
+		super();
+		this.uid = uid;
+		this.userName = userName;
+		this.password = password;
+		this.role = role;
+	}
+	public Long getUid() {
+		return uid;
+	}
+	public void setUid(Long uid) {
+		this.uid = uid;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
 }
--- src/main/java/com/rak/assignment/to/UserTo.java
+++ src/main/java/com/rak/assignment/to/UserTo.java
@@ -1,16 +1,32 @@
 package com.rak.assignment.to;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-@Setter
-@Getter
-@NoArgsConstructor
-@AllArgsConstructor
 public class UserTo {
 	private String userName;
 	private String password;
 	private String role;
+	public UserTo(){}
+	public UserTo(String userName, String password, String role) {
+		super();
+		this.userName = userName;
+		this.password = password;
+		this.role = role;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
 }
